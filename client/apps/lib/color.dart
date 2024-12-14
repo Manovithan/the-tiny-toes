92a9f1de232ce7b853e3eaa83a9131391003d14d
Manovithan <manovithan49@gmail.com>
2024-12-14 23:34:47 +0530

    # 5.1 login screen created

---
+++ client/apps/lib/color.dart
@@ -0,0 +1,6 @@
+import 'package:apps/material.dart';
+import 'package:flutter/material.dart';
+
+const Color appBar = Color(0xFFF8BBD0);
+const Color primaryColor = Color(0xFFC51162);
+const Color icons = Color(0xFFC51162);
---
+++ client/apps/lib/constants.dart
@@ -0,0 +1,4 @@
+const String appName = "the tiny toes";
+const String bgImage = "asset/login_bg2.jpg";
+const String loginString = "LOGIN";
+const String loginbutton = "Login to account";
